1bc481c6ddc6609ad3cb461854dad084ad47b29c
Isarm <44964944+Isarm@users.noreply.github.com>
2018-11-22 14:01:07 +0100

    Update video_system-structural.vhd

--- video_system/video_system-structural.vhd
+++ video_system/video_system-structural.vhd
@@ -19,17 +19,33 @@ component vga_controll is
 end component vga_controll;
 
 component video_control is
-   port(clk          : in  std_logic;
-        reset        : in  std_logic;
-        sync         : in  std_logic;
-        cell_type    : in  std_logic_vector(2 downto 0);
-        sprite_colour: in  std_logic_vector(2 downto 0);
-        pixel_array  : in  std_logic_vector(7 downto 0);
-        sprite_type  : out std_logic_vector(2 downto 0);
-        y_pos        : out std_logic_vector(2 downto 0);
-        colour       : out std_logic_vector(2 downto 0);
-        xcoordinates : out std_logic_vector(4 downto 0);
-	ycoordinates : out std_logic_vector(4 downto 0));
+   port(clk           : in  std_logic;
+        reset         : in  std_logic;
+        sync          : in  std_logic;
+        cell_type     : in  std_logic_vector(2 downto 0);
+        sprite_colour : in  std_logic_vector(2 downto 0);
+	pixel_array   : in  std_logic_vector(7 downto 0);
+        sprite_type   : out std_logic_vector(2 downto 0);
+	y_pos	      : out std_logic_vector(2 downto 0);
+	colour	      : out std_logic_vector(2 downto 0);
+	xcoordinates  : out std_logic_vector(4 downto 0);
+	ycoordinates  : out std_logic_vector(4 downto 0);
+	
+	county		: in std_logic_vector(2 downto 0);
+	dual_pixel_y		: in std_logic;
+	current_block_horizontal : in std_logic_vector(4 downto 0);
+	current_block_vertical   : in std_logic_vector(4 downto 0);
+
+	reset_dual_pixel_y			: out std_logic;
+	reset_current_block_horizontal		: out std_logic;
+	reset_current_block_vertical		: out std_logic;
+	reset_county				: out std_logic;
+
+	en_county				: out std_logic;
+	en_current_block_horizontal		: out std_logic;
+	en_current_block_vertical		: out std_logic;
+	en_dual_pixel_y				: out std_logic
+       );
 end component video_control;
 
 component sprite is
@@ -39,6 +55,29 @@ component sprite is
 		row_out		: out std_logic_vector (7 downto 0)  --left to right		
 		);
 end component sprite;
+	
+component counter is
+
+	port(
+	clk		: in  std_logic;
+	county		: out std_logic_vector(2 downto 0);
+	dual_pixel_y		: out std_logic;
+	current_block_horizontal : out std_logic_vector(4 downto 0);
+	current_block_vertical   : out std_logic_vector(4 downto 0);
+
+	reset_dual_pixel_y			: in std_logic;
+	reset_current_block_horizontal		: in std_logic;
+	reset_current_block_vertical		: in std_logic;
+	reset_county				: in std_logic;
+
+	en_county				: in std_logic;
+	en_current_block_horizontal		: in std_logic;
+	en_current_block_vertical		: in std_logic;
+	en_dual_pixel_y				: in std_logic);
+
+end component;
+	
+
 
 signal y_pos,sprite_type, colour, sprite_colour: std_logic_vector(2 downto 0);
 signal sync: std_logic;
