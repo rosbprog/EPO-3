8a382c496a081b646d31c47a2c13ac5b3be69ba7
rubenwijnands999 <44965015+rubenwijnands999@users.noreply.github.com>
2018-11-26 20:02:19 +0100

    Update score_count_3x4bits_behaviour.vhdl

--- score_counter/score_count_3x4bits_behaviour.vhdl
+++ score_counter/score_count_3x4bits_behaviour.vhdl
@@ -5,7 +5,6 @@ use ieee.numeric_std.all;
 architecture behaviour of score_count is
 
 signal in_a, in_b, in_c, new_a, new_b, new_c: unsigned(3 downto 0);
-signal supp_a, supp_b: std_logic;
 
 begin
 L1: process(clk,reset)
@@ -26,42 +25,22 @@ end process;
 L2: process(plus)
 begin
 	if (plus='1') then
-		if (in_a<"1001" and supp_a='0') then
+		if (in_a<"1001") then
 			new_a<=in_a+1;
 			new_b<=in_b;
 			new_c<=in_c;
-			supp_a<='0';
-			supp_b<='0';
-		elsif (in_a="1001" and supp_a='0') then
+		elsif (in_a="1001" and in_b<"1001") then
 			new_a<="0000";
 			new_b<=in_b+1;
 			new_c<=in_c;
-			supp_a<='1';
-			supp_b<='0';
-		elsif (in_b<"1001" and supp_b='0') then
-			new_a<=in_a;
-			new_b<=in_b+1;
-			new_c<=in_c;
-			supp_a<='0';
-			supp_b<='0';
-		elsif (in_b="1001" and supp_b='0') then
+		elsif (in_b="1001" and in_c<"1001" and in_a="1001") then
 			new_a<=in_a;
 			new_b<="0000";
-			new_c<=in_c;
-			supp_a<='0';
-			supp_b<='1';
-		elsif (in_c<"1001" and supp_a='1' and supp_b='1') then
-			new_a<=in_a;
-			new_b<=in_b;
 			new_c<=in_c+1;
-			supp_a<='1';
-			supp_b<='1';
-		elsif(in_c="1111") then
-			new_a<="0000";
-			new_b<="0000";
-			new_c<="0000";
-			supp_a<='0';
-			supp_b<='0';
+		else
+			in_a<="0000";
+			in_b<="0000";
+			in_c<="0000";
 		end if;
 	else
 		new_a<=in_a;
