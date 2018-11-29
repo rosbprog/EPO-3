b66f1148ec0f96037977223cd5679fc9e07e119b
Roy Bakker <roysbakker@gmail.com>
2018-11-29 11:44:36 +0100

    Add files via upload

--- coordinate_adder/coord_adder_arch.vhd
+++ coordinate_adder/coord_adder_arch.vhd
@@ -2,18 +2,15 @@ architecture behavioural of coordinate_adder is
 begin
 
 	---------------------------------------------------------------------------------------------------------
-	-- clk_slow_proc:
-	-- On rising edge of the "clk_slow" signal, calculate the new cell coordinates
-	--	by moving from the original cell
+	-- calc_proc:
+	-- Calculate the new cell coordinates by moving from the original cell
 	---------------------------------------------------------------------------------------------------------
-	clk_slow_proc: process(clk_slow)
+	calc_proc: process(row_number_old, column_number_old)
 	begin
-		if rising_edge(clk_slow) then
-			row_number_new 		<= std_logic_vector(unsigned(row_number_old) -
-				move(1) + move(3));
-			column_number_new 	<= std_logic_vector(unsigned(column_number_old) -
-				move(2) + move(0));
-		end if;
+		row_number_new 		<= std_logic_vector(unsigned(row_number_old) -
+			move(1) + move(3));
+		column_number_new 	<= std_logic_vector(unsigned(column_number_old) -
+			move(2) + move(0));
 	end process;
 
 end architecture behavioural;
