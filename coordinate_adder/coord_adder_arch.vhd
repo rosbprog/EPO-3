ee3de6c6c6af2f5964e6fed426325000916f9497
Roy <roysbakker@gmail.com>
2018-11-29 13:26:39 +0100

    Modified the coordinate adder component

--- coordinate_adder/coord_adder_arch.vhd
+++ coordinate_adder/coord_adder_arch.vhd
@@ -1,19 +1,29 @@
-architecture behavioural of coordinate_adder is
+architecture behavioural_case of coordinate_adder is
 begin
 
 	---------------------------------------------------------------------------------------------------------
-	-- clk_slow_proc:
-	-- On rising edge of the "clk_slow" signal, calculate the new cell coordinates
-	--	by moving from the original cell
+	-- calc_proc:
+	-- Calculate the new cell coordinates by moving from the original cell
 	---------------------------------------------------------------------------------------------------------
-	clk_slow_proc: process(clk_slow)
+	calc_proc: process(row_number_old, column_number_old)
 	begin
-		if rising_edge(clk_slow) then
-			row_number_new 		<= std_logic_vector(unsigned(row_number_old) -
-				move(1) + move(3));
-			column_number_new 	<= std_logic_vector(unsigned(column_number_old) -
-				move(2) + move(0));
-		end if;
+		case move is
+			when "0001" =>
+				row_number_new		<= row_number_old;
+				column_number_new	<= std_logic_vector(unsigned(column_number_old) + 1);
+			when "0010" =>
+				row_number_new		<= std_logic_vector(unsigned(row_number_old) - 1);
+				column_number_new	<= column_number_old;
+			when "0100" =>
+				row_number_new		<= row_number_old;
+				column_number_new	<= std_logic_vector(unsigned(column_number_old) - 1);
+			when "1000" =>
+				row_number_new		<= std_logic_vector(unsigned(row_number_old) + 1);
+				column_number_new	<= column_number_old;
+			when others =>
+				row_number_new		<= row_number_old;
+				column_number_new	<= column_number_old;
+		end case;
 	end process;
 
-end architecture behavioural;
+end architecture behavioural_case;
