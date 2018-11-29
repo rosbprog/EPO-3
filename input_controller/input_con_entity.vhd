c4ca8520687f604db60717ddcc29c65b2711b060
Roy Bakker <roysbakker@gmail.com>
2018-11-29 10:46:09 +0100

    Added all files for the input system

---
+++ input_controller/input_con_entity.vhd
@@ -0,0 +1,9 @@
+use IEEE.std_logic_1164.all;
+
+entity input_controller is
+	port (	clk			: in	std_logic;
+		reset			: in	std_logic;
+
+		move_in			: in	std_logic_vector(3 downto 0);
+		move_out		: out	std_logic_vector(3 downto 0));
+end entity input_controller
