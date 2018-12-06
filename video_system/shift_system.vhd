d9f14f1dcc60421c5efed862322f2e69e22a577f
Isarm <44964944+Isarm@users.noreply.github.com>
2018-12-06 10:59:31 +0100

    Update shift_system.vhd

--- video_system/shift_system.vhd
+++ video_system/shift_system.vhd
@@ -10,11 +10,8 @@ entity shift_system is
         cell_state_in : in  std_logic_vector(2 downto 0);
         y_pos_in      : in  std_logic_vector(2 downto 0);
         pixel_arr_in  : in  std_logic_vector(7 downto 0);
-	sprite_colour_in     : in  std_logic_vector(2 downto 0);
 	
 	screen_sync	  : in std_logic;
-	
-	sprite_colour_out : out std_logic(2 downto 0);
 
 	cell_state_out: out std_logic_vector(2 downto 0);
         y_pos_out     : out std_logic_vector(2 downto 0);
