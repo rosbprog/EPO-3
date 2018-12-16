86cd3c9e1ed669a73a83332a1eae205b6226fa26
D1mme <dimmedeg@gmail.com>
2018-12-16 15:09:02 +0100

    Delete speedcontroller_counter_entity.vhd

--- counter_ghostpac_speeedddd/speedcontroller_counter_entity.vhd
+++
@@ -1,9 +0,0 @@
-library IEEE;
-use IEEE.std_logic_1164.all;
-use IEEE.numeric_std.all;
-
-entity counter is
-	port(	vc_done		: in	std_logic;
-		c_rst		: in 	std_logic; 
-		count_out 	: out 	std_logic_vector(6 downto 0));
-end entity counter;
