67f98186d9893fcea05607dafdf063cf1a791c36
D1mme <dimmedeg@gmail.com>
2018-12-17 18:35:45 +0100

    Update speedcontroller_register_entity.vhd

--- speedcontroller/speedcontroller_register_entity.vhd
+++ speedcontroller/speedcontroller_register_entity.vhd
@@ -4,7 +4,7 @@ use IEEE.numeric_std.all;
 
 entity register_7bit is
 	port(	clk		: in	std_logic;
-		count_in		: in	std_logic_vector(6 downto 0);
+		count_in	: in	std_logic_vector(6 downto 0);
 		c_rst		: in 	std_logic; 
-		count_out 		: out 	std_logic_vector(6 downto 0));
+		count_out 	: out 	std_logic_vector(6 downto 0));
 end entity register_7bit;
