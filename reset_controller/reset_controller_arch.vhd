9c00489b40f4c0b7fb16fdc48f608862d77dd626
Roy Bakker <roysbakker@gmail.com>
2018-12-18 10:02:15 +0100

    Update reset_controller_arch.vhd

--- reset_controller/reset_controller_arch.vhd
+++ reset_controller/reset_controller_arch.vhd
@@ -1,10 +1,10 @@
 architecture behavioural of reset_controller is
 begin
 
-	input_controller_reset		<= '1' when pacman_rip = '1' or zero_coins = '1' else '0';
 	coin_register_reset		<= '1' when pacman_rip = '1' or zero_coins = '1' else '0';
 	score_counter_reset		<= '1' when pacman_rip = '1' else '0';
-	fsm_reset			<= '1' when pacman_rip = '1' or zero_coins = '1' else '0';
-	coordinate_register_reset	<= '1' when pacman_rip = '1' or zero_coins = '1' else '0';
+	pacman_reset			<= '1' when pacman_rip = '1' else '0';
+	ghost1_reset			<= '1' when pacman_rip = '1' else '0';
+	ghost2_reset			<= '1' when pacman_rip = '1' else '0';
 
 end architecture behavioural;
