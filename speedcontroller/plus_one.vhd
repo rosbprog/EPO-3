30c7f2a2c92359ffac5e833cf0265b8fa4492dcb
D1mme <dimmedeg@gmail.com>
2018-12-20 10:14:13 +0100

    Delete plus_one.vhd

--- speedcontroller/plus_one.vhd
+++
@@ -1,9 +0,0 @@
-library IEEE;
-use IEEE.std_logic_1164.all;
-use IEEE.numeric_std.all;
-
-entity plus_one is
-   port(count_in  : in  std_logic_vector(6 downto 0);
-        count_out : out std_logic_vector(6 downto 0));
-end plus_one;
-
