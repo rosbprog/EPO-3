79195abc51c363a0d534c125310a59bdff03404a
rubenwijnands999 <44965015+rubenwijnands999@users.noreply.github.com>
2018-12-20 11:08:45 +0100

    Update video_system-structural.vhd

--- video_system/video_system-structural.vhd
+++ video_system/video_system-structural.vhd
@@ -22,7 +22,8 @@ architecture structural of video_system is
    
    		en_county				: out std_logic;
    		en_current_block_horizontal				: out std_logic;
-   		en_dual_pixel_y				: out std_logic
+   		en_dual_pixel_y				: out std_logic;
+	 	score_12bits				: out std_logic_vector(11 downto 0)
    	);
    
    end component;
@@ -40,7 +41,8 @@ component vga_controll is
         blue         : out std_logic;
         h_sync       : out std_logic;
         v_sync       : out std_logic;
-	calc_start	  : out std_logic);
+	calc_start   : out std_logic;
+        go_pixel_sync: out std_logic);
 end component vga_controll;
 
 component video_control is
@@ -121,6 +123,59 @@ component shift_system is
 
 );
 end component;
+	
+component screen_controller is
+  port (clk	  : in std_logic;
+	reset	  : in std_logic;
+        user      : in std_logic;
+        game_over : in std_logic;
+        mux_sel   : out std_logic;
+        st_go_sel : out std_logic
+	);
+end component;
+	
+component gameover_control is
+ port( 	clk       				: in std_logic;
+        reset     					: in std_logic;
+        score					: in std_logic_vector(11 downto 0);
+        gameover_sync_vga          					: in std_logic;
+
+        start_go_sel					: in std_logic;
+	pixel_array				: in std_logic_vector(7 downto 0);
+
+	dual_pixel_y				: in std_logic;
+	county				: in std_logic_vector(2 downto 0);
+	current_block_horizontal 				: in std_logic_vector(4 downto 0);
+
+	go_colour       				: out std_logic_vector(2 downto 0);
+	go_sprite_type				: out std_logic_vector(4 downto 0);
+	go_y_pos				: out std_logic_vector(2 downto 0);
+
+	reset_dual_pixel_y				: out std_logic;
+	reset_current_block_horizontal		: out std_logic;
+	reset_county				: out std_logic;
+
+	en_county				: out std_logic;
+	en_current_block_horizontal				: out std_logic;
+	en_dual_pixel_y				: out std_logic
+	);
+
+end component;
+
+component character_sprite is
+	port(  	y_pos			:in std_logic_vector (2 downto 0);
+		--Upper row is 000, lower is 111
+	 	char_sprite_select	:in std_logic_vector (4 downto 0);
+		--00000/01100 is 0, 00001 is 1, 00010 is 2, 00011 is 3, 00100 is 4, 00101 is 5
+		--00110 is 6, 00111 is 7, 01000 is 8, 01001 is 9, 01010 is S, 01011 is C,
+		--01101 is R, 01110 is E, 01111 is ':', 10000 is G, 10001 is A,
+    --10010 is M, 10011 is T, 10100 is V, 10101 is empty (ie black) square,
+    --10110 is P, 10111 is N, 11000 is '-'
+
+		row_out			:out std_logic_vector (7 downto 0)
+		--right to left
+		);
+end component;
 
 signal cell_type_test: std_logic_vector(2 downto 0);
 
@@ -131,7 +186,13 @@ signal	county:  std_logic_vector(2 downto 0);
 signal	dual_pixel_y: std_logic;
 signal	current_block_horizontal, ycoordinates_internal, xcoordinates_internal: std_logic_vector(4 downto 0);
 signal	current_block_vertical :  std_logic_vector(4 downto 0);
-signal	reset_dual_pixel_y, reset_dual_pixel_y_score, reset_dual_pixel_y_video, reset_current_block_horizontal, reset_current_block_horizontal_score, reset_current_block_horizontal_video, reset_current_block_vertical, reset_county,	reset_county_score, reset_county_video, en_county, en_county_score, en_county_video, en_current_block_horizontal, en_current_block_horizontal_score, en_current_block_horizontal_video, en_current_block_vertical, en_dual_pixel_y, en_dual_pixel_y_score, en_dual_pixel_y_video: std_logic;	
+signal	reset_dual_pixel_y, reset_dual_pixel_y_score, reset_dual_pixel_y_video, reset_dual_pixel_y_go, reset_current_block_horizontal, reset_current_block_horizontal_score, reset_current_block_horizontal_video, reset_current_block_horizontal_go, reset_current_block_vertical, reset_county,reset_county_score, reset_county_video, reset_county_go, en_county, en_county_score, en_county_video, en_county_go, en_current_block_horizontal, en_current_block_horizontal_score, en_current_block_horizontal_video,en_current_block_horizontal_go, en_current_block_vertical, en_dual_pixel_y, en_dual_pixel_y_score, en_dual_pixel_y_video,en_dual_pixel_y_go: std_logic;	
+signal in_mux_sel, in_st_go_sel: std_logic;
+signal in_score_12bits: std_logic_vector(11 downto 0);
+signal in_go_pixel_sync: std_logic;
+signal in_row_out: std_logic_vector(7 downto 0);
+signal in_go_colour, in_go_y_pos: std_logic_vector(2 downto 0);
+signal in_go_sprite_type: std_logic_vector(4 downto 0);
 
 begin
 
@@ -145,25 +206,31 @@ sprites: sprite port map(y_pos_shifted, sprite_type_to_register, sprite_colour, 
 shift: shift_system port map (clk, reset, xcoordinates_internal, ycoordinates_internal, sprite_type_to_shift, y_pos_to_shift, pixel_array_to_shift, calc_start_internal, sprite_type_to_register, y_pos_shifted, pixel_array_shifted
 );
 
-vgacontrol: vga_controll port map(clk, reset, colour, sync, score_sync, red, green, blue, h_sync, v_sync, calc_start_internal);
+vgacontrol: vga_controll port map(clk, reset, colour, sync, score_sync, red, green, blue, h_sync, v_sync, calc_start_internal, in_go_pixel_sync);
 
 cnt: counter port map( clk, county, dual_pixel_y, current_block_horizontal, current_block_vertical, 
 			 reset_dual_pixel_y, reset_current_block_horizontal, reset_current_block_vertical, reset_county,
 			 en_county, en_current_block_horizontal, en_current_block_vertical, en_dual_pixel_y);
 
-score: score_system port map( clk, reset, score_plus, score_sync, dual_pixel_y, county, current_block_horizontal, rgb_score, reset_dual_pixel_y_score, reset_current_block_horizontal_score, reset_county_score, en_county_score, en_current_block_horizontal_score, en_dual_pixel_y_score);
+score: score_system port map( clk, reset, score_plus, score_sync, dual_pixel_y, county, current_block_horizontal, rgb_score, reset_dual_pixel_y_score, reset_current_block_horizontal_score, reset_county_score, en_county_score, en_current_block_horizontal_score, en_dual_pixel_y_score,in_score_12bits);
+	
+screencontrol: screen_controller port map(clk,reset,user_begin,in_mux_sel,in_st_go_sel);
+	
+gameovercontrol: gameover_control port map(clk,reset,in_score_12bits,in_go_pixel_sync,in_st_go_sel,in_row_out,dual_pixel_y, county, current_block_horizontal,in_go_colour,in_go_sprite_type,in_go_y_pos,reset_dual_pixel_y_go, reset_current_block_horizontal_go, reset_county_go, en_county_go, en_current_block_horizontal_go, en_dual_pixel_y_go);
+	
+spirtego: character_sprite port map(in_go_y_pos,in_go_sprite_type,in_row_out);
 
 colour(0)<= rgb_score(0) OR  rgb_video(0);
 colour(1)<= rgb_score(1) OR  rgb_video(1);
 colour(2)<= rgb_score(2) OR  rgb_video(2);
 
-reset_dual_pixel_y <= reset_dual_pixel_y_score OR reset_dual_pixel_y_video;
-reset_current_block_horizontal <= reset_current_block_horizontal_score OR reset_current_block_horizontal_video;
-reset_county <= reset_county_score OR reset_county_video; 
+reset_dual_pixel_y <= reset_dual_pixel_y_score OR reset_dual_pixel_y_video OR reset_dual_pixel_y_go;
+reset_current_block_horizontal <= reset_current_block_horizontal_score OR reset_current_block_horizontal_video OR reset_current_block_horizontal_go;
+reset_county <= reset_county_score OR reset_county_video OR reset_county_go; 
 
-en_dual_pixel_y <= en_dual_pixel_y_score OR en_dual_pixel_y_video;
-en_current_block_horizontal <= en_current_block_horizontal_score OR en_current_block_horizontal_video;
-en_county <= en_county_score OR en_county_video; 
+en_dual_pixel_y <= en_dual_pixel_y_score OR en_dual_pixel_y_video OR en_dual_pixel_y_go;
+en_current_block_horizontal <= en_current_block_horizontal_score OR en_current_block_horizontal_video OR en_current_block_horizontal_go;
+en_county <= en_county_score OR en_county_video OR en_county_go; 
 
 
 xcoordinates <= xcoordinates_internal;
@@ -182,6 +249,14 @@ begin
 	end if;
 end process;
 
+process(colour, in_go_colour, in_mux_sel)
+begin
+	case in_mux_sel is
+		when '0'=> rgb_out <= in_go_colour;
+		when '1'=> rgb_out <= colour;
+		when others => rgb_out <= colour;
+	end case;
+end process;
 
 
 end architecture structural;
