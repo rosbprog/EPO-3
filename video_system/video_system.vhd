a3cba5b12b260029e331f57611297b715e7dff8f
rubenwijnands999 <44965015+rubenwijnands999@users.noreply.github.com>
2018-12-20 11:26:17 +0100

    Update video_system.vhd

--- video_system/video_system.vhd
+++ video_system/video_system.vhd
@@ -8,6 +8,7 @@ entity video_system is
         reset     : in std_logic;
         cell_type : in std_logic_vector(2 downto 0);
 	score_plus : in std_logic;
+       	user_begin : in std_logic;
         xcoordinates : out std_logic_vector(4 downto 0);
         ycoordinates : out std_logic_vector(4 downto 0);
         red       : out std_logic;
