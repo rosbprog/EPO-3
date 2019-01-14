583f2ecd1211f674d5c78b166af22b0c8ab6a373
acneaguu <45164974+acneaguu@users.noreply.github.com>
2019-01-14 10:19:13 +0100

    Update sprites.vhd

--- merged_system/sprites.vhd
+++ merged_system/sprites.vhd
@@ -10,8 +10,8 @@ entity sprites is
 	--next 2 bits indicate the orientation of Pac-Man:
 	--10 is right, 01 is down, 00 is left, 11 is up
 	--last 3 bits indicate the game sprite which is to be selected
-	--001 pacman, 010 ghost_red, 011 ghost_green, 101 coin, 110 wall
-	--000/100/111 becomes open i.e. black square
+	--001 pacman, 010 ghost_red, 011 ghost_green, 100 is power-up, 101 coin, 110 wall
+	--000/111 becomes open i.e. black square
 
 	--selection of a sprite is done first;
 	--if this is not Pac-Man, then the mouth and orientation information remain unused
